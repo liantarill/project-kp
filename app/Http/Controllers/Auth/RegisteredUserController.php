3291845bcd365fb4499ce28a7c803468430ce858
liantarill <yusufherliananantaril@gmail.com>
2025-12-28 00:55:03 +0700

    update register

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Department;
+use App\Models\Institution;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
@@ -19,7 +21,10 @@ class RegisteredUserController extends Controller
      */
     public function create(): View
     {
-        return view('auth.register');
+        $departments = Department::orderBy('name')->get();
+        $institutions = Institution::orderBy('name')->get();
+
+        return view('auth.register', compact('departments', 'institutions'));
     }
 
     /**
@@ -33,24 +38,33 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            // 'institution' => ['required'],
-            // 'major' => ['required'],
-            // 'department' => ['required'],
-            // 'acceptance_proof' => ['required'],
-            // 'start_date' => ['required'],
-            // 'end_date' => ['required'],
+            'institution_id' => ['nullable', 'exists:institutions,id'],
+            'major' => ['required', 'string', 'max:255'],
+            'level' => ['required', 'in:SMA,SMK,D1,D2,D3,D4,S1'],
+            'department_id' => ['required', 'exists:departments,id'],
+            'acceptance_proof' => ['required', 'file', 'mimes:pdf,jpg,jpeg,png', 'max:2048'],
+            'start_date' => ['required', 'date'],
+            'end_date' => ['required', 'date', 'after_or_equal:start_date'],
         ]);
+
+        $acceptanceProofPath = null;
+
+        if ($request->hasFile('acceptance_proof')) {
+            $acceptanceProofPath = $request->file('acceptance_proof')
+                ->store('acceptance-proofs', 'public');
+        }
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            // 'institution' => $request->institution,
-            // 'major' => $request->major,
-            // 'department' => $request->department,
-            // 'acceptance_proof' => $request->acceptance_proof,
-            // 'start_date' => $request->start_date,
-            // 'end_date' => $request->end_date,
+            'institution_id' => $request->institution_id,
+            'major' => $request->major,
+            'level' => $request->level,
+            'department_id' => $request->department_id,
+            'acceptance_proof' => $acceptanceProofPath,
+            'start_date' => $request->start_date,
+            'end_date' => $request->end_date,
         ]);
 
         event(new Registered($user));
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,14 +5,16 @@
         <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
+                autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
@@ -20,10 +22,8 @@
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
@@ -32,15 +32,74 @@
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
+                name="password_confirmation" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
+        <!-- Institutions -->
+        <div class="mt-4">
+            <x-input-label for="institution" :value="__('Asal Instansi')" />
+            <select name="institution_id" id="institution_id" required>
+                <option value="">-- Pilih Departemen --</option>
+
+                @foreach ($institutions as $institution)
+                    <option value="{{ $institution->id }}">
+                        {{ $institution->name }}
+                    </option>
+                @endforeach
+            </select>
+            <x-input-error :messages="$errors->get('institution')" class="mt-2" />
+        </div>
+
+        <div class="mt-4">
+            <x-input-label for="major" :value="__('Jurusan')" />
+            <x-text-input id="major" class="block mt-1 w-full" type="text" name="major" :value="old('major')"
+                required autocomplete="" />
+            <x-input-error :messages="$errors->get('major')" class="mt-2" />
+        </div>
+
+        <div class="mt-4">
+            <x-input-label for="level" :value="__('Jenjang')" />
+            <x-text-input id="level" class="block mt-1 w-full" type="text" name="level" :value="old('level')"
+                required autocomplete="" />
+            <x-input-error :messages="$errors->get('level')" class="mt-2" />
+        </div>
+
+        <div class="mt-4">
+            <x-input-label for="department_id" :value="__('Divisi')" />
+
+            <select name="department_id" id="department_id" required>
+                <option value="">-- Pilih Departemen --</option>
+
+                @foreach ($departments as $department)
+                    <option value="{{ $department->id }}">
+                        {{ $department->name }}
+                    </option>
+                @endforeach
+            </select>
+
+            <x-input-error :messages="$errors->get('department_id')" class="mt-2" />
+        </div>
+
+        <div class="mt-4">
+            <x-input-label for="start_date" :value="__('Tanggal Mulai')" />
+            <x-text-input id="start_date" class="block mt-1 w-full" type="date" name="start_date" :value="old('start_date')"
+                required autocomplete="" />
+            <x-input-error :messages="$errors->get('start_date')" class="mt-2" />
+        </div>
+
+        <div class="mt-4">
+            <x-input-label for="end_date" :value="__('Tanggal Mulai')" />
+            <x-text-input id="end_date" class="block mt-1 w-full" type="date" name="end_date" :value="old('end_date')"
+                required autocomplete="" />
+            <x-input-error :messages="$errors->get('end_date')" class="mt-2" />
+        </div>
+
         <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
 
